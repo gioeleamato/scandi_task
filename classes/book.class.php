dfd4bff81c005f8ac1bbe3512e30d1c216fda7ac
gioeleamato <99969982+gioeleamato@users.noreply.github.com>
2022-08-11 00:19:33 +0300

    Add files via upload

--- classes/book.class.php
+++ classes/book.class.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Book is the class that represent the book category.
+ * The class has 6 properties and 3 methods.
+ */
 class Book extends ProductController
 {
     private $sku;
@@ -7,28 +11,31 @@ class Book extends ProductController
     private $price;
     private $category;
     private $bookWeight;
-    private $unit="MB";  //MB
-
-    private $weight;    //Kg
+    private $unit="Kg";
     
-
     public function __construct($sku, $name, $price, $category, $bookWeight)
     {
-        $this->sku=$sku;
-        $this->name=$name;
-        $this->price=$price;
-        $this->category=$category;
-        $this->bookWeight=$bookWeight;
+        $this->sku = $sku;
+        $this->name = $name;
+        $this->price = $price;
+        $this->category = $category;
+        $this->bookWeight = $bookWeight;
     }
 
     private function composeSpecialAttribute()
     {
-        $special=$this->bookWeight;
+        $special = $this->bookWeight;
         return $special." Kg";
     }
 
     public function saveMe()
     {
-        $this->saveProduct($this->sku, $this->name, $this->price, $this->category,$this->composeSpecialAttribute());
+        $this->saveProduct(
+            $this->sku,
+            $this->name,
+            $this->price,
+            $this->category,
+            $this->composeSpecialAttribute()
+        );
     }
 }
